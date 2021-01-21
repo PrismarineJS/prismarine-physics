91f9986a946cad85d1043b1be08b0cc0e7a0c9e4
Karang <arthur.hennequin@free.fr>
2021-01-21 21:59:18 +0100

    Fix airdrag value

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ function Physics (mcData, world) {
 
   const physics = {
     gravity: 0.08, // blocks/tick^2 https://minecraft.gamepedia.com/Entity#Motion_of_entities
-    airdrag: (1 - 0.02), // actually (1 - drag)
+    airdrag: 0.9800000190734863, // actually (1 - drag)
     yawSpeed: 3.0,
     sprintSpeed: 1.3,
     sneakSpeed: 0.3,
