aa1df1ad4c84c1eb8d7526efec88bbdcec4d09c0
usb <56985400+U5B@users.noreply.github.com>
2021-10-10 10:17:10 -0700

    From WhoTho - fix speed and slowness effect
    https://github.com/PrismarineJS/mineflayer/issues/2241#issuecomment-939384631

--- index.js
+++ index.js
@@ -77,8 +77,8 @@ function Physics (mcData, world) {
       maxUp: 0.7
     },
     slowFalling: 0.125,
-    speedEffect: 1.2,
-    slowEffect: 0.85
+    speedEffect: 0.2,
+    slowEffect: 0.15
   }
 
   if (supportFeature('independentLiquidGravity')) {
@@ -382,8 +382,8 @@ function Physics (mcData, world) {
         acceleration = 0.1 * (0.1627714 / (inertia * inertia * inertia))
       }
       if (entity.control.sprint) acceleration *= physics.sprintSpeed
-      if (entity.speed > 0) acceleration *= physics.speedEffect * entity.speed
-      if (entity.slowness > 0) acceleration *= physics.slowEffect * entity.slowness
+      if (entity.speed > 0  && entity.onGround) acceleration *= 1 + (physics.speedEffect * entity.speed)
+      if (entity.slowness > 0) acceleration *= Math.max(1 - (physics.slowEffect * entity.slowness), 0)
 
       applyHeading(entity, strafe, forward, acceleration)
 
